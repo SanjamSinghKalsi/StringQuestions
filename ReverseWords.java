6cb973cb44ba2b3bd030da51bc356a35c9bfc1cd
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2022-11-25 22:14:38 +0530

    Create ReverseWords.java

---
+++ ReverseWords.java
@@ -0,0 +1,6 @@
+ static String reverseWords(String string) {
+        StringBuilder answer = new StringBuilder(string);
+        //String answer = "";
+       answer.reverse();
+        return answer.toString();
+    }
